d5a655925d34bf2d29b8d45612cd26d01543108f
catalin.festila <catafest76@gmail.com>
2022-04-08 12:40:30 +0300

    start nextjs ...

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    //domains: ['bit.ly'],
+    /**
+	  * ! Highly suggested to have your max content width defined here (it will better optimize the image size)
+	  * !   For example, on my website, an image is at most 992px which is the centered part of the viewport where I put content
+	  * ! Makes sure values here are in sync with `helper/Image.ts`
+    * ! see also this link : 
+	  * ! https://www.onely.com/blog/largest-contentful-paint/
+    */
+	//deviceSizes: [320, 480, 640, 750, 828, 992, 1080, 1200, 1440, 1920, 2048, 2560, 3840],
+	domains: ['img.icons8.com'],
+  //imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+	//formats: ['image/avif', 'image/webp'],
+	//minimumCacheTTL: 86400 // if `no max-age` or `s-max-age` defined for an image, cache it `1 day`
+  },
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,92 @@
-import type { NextPage } from 'next'
+import Image from 'next/image';
+import { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+
+export async function getStaticProps(){
+const supabaseAdmin = createClient(
+  /*process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ''*/
+  'https://dmhzwruowsngalaozvlh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtaHp3cnVvd3NuZ2FsYW96dmxoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0OTQwNTE0MCwiZXhwIjoxOTY0OTgxMTQwfQ.LGC8iuiREzK7lMTWfGJF0N_fFvUwz-L2UW3cxo5T0DI'
+  )
+
+  const { data } = await supabaseAdmin.from('images').select('*').order('id')
+  return {
+    props: {
+      images: data,
+    },
+  }
+}
+
+function cn(...classes: string[])
+{
+  return classes.filter(Boolean).join(' ');
+}
+
+// define the Image type
+type Image = {
+  id: number,
+  href: string,
+  imageSrc: string,
+  name: string,
+  usernames: string
+}
+
+// define a type like: {images}: {images: Image[]  } for image from supabase and need to update and iterate through these images
+export default function Gallery({images}: {images: Image[]  }) {
+  //return 'Hello world!'
+  return (
+    <div className="max-w-2xl mx-auto py-16 px-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {images.map((image) => (
+          <BlurImage key={image.id} image={image} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+// changes to Image type is send to BlurImage like arg : {image}): {image: Image}
+function BlurImage({ image }: {image: Image}) 
+{
+  const [isLoading, setLoading] = useState(true)
+  return (
+    <a href={image.href} className="group">
+      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+        <Image 
+        alt=""
+        //src="http://bit.ly/placeholder-img" 
+        src={image.imageSrc}
+        layout="fill"
+        objectFit="cover"
+        className={cn("group-hover:opacity-75 duration-700 ease-in-out", 
+          isLoading 
+          ? 'grayscale blur-2xl scale-110'
+          : 'grayscale-0 blur-0 scale-100'
+        )}
+      onLoadingComplete={() => setLoading(false)}
+      />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
+    </a>
+  )
+}
+
+/*
+function Image() {
+  return (
+    <a href="#" className="group">
+      <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+        <img alt="" src="http://bit.ly/placeholder-img" className="group-hover:opacity-75" />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
+    </a>
+  )
+}
+*/
+
+/*import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -84,3 +172,4 @@ const Home: NextPage = () => {
 }
 
 export default Home
+*/
